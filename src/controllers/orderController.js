32ba657b6ee709550b067662744149b87544694d
Tusharg376 <honeygautam0@gmail.com>
2023-02-04 23:13:13 +0530

    project done

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -20,22 +20,22 @@ module.exports.createOrder = async function(req,res){
         let orderData = await orderModel.create(data)
     
         let {numberOfOrders,moneyBack} = categorycheck
-        
+
         if(numberOfOrders<10 || typeof(numberOfOrders) == "undefined") {
             let orderRemaining = (9-numberOfOrders)
             await customerModel.findOneAndUpdate({_id:userId},{$inc:{numberOfOrders:1}})
-            return res.status(201).send({status:true,message:`Order placed, you are ${orderRemaining} orders behind to become a gold member and enjoing 10% discount`,orderDetails:orderData})
+            return res.status(201).send({status:true,message:`Order placed, you are ${orderRemaining} orders behind to become a gold member and enjoying 10% discount`,orderDetails:orderData})
         }
         if(numberOfOrders>=10 && numberOfOrders<20){
             let orderRemaining = 19-numberOfOrders
             moneyBack = moneyBack+(price*10/100)
             await customerModel.findOneAndUpdate({_id:userId},{$set:{moneyBack:moneyBack,category:"gold"},$inc:{numberOfOrders:1}})
-            return res.status(201).send({status:true,message:`Order placed, you are ${orderRemaining} orders behind to become a platinum member and enjoing 20% discount`,orderDetails:orderData})
+            return res.status(201).send({status:true,message:`Order placed, you are ${orderRemaining} orders behind to become a platinum member and enjoying 20% discount`,orderDetails:orderData})
         }
         if(numberOfOrders>=20){
             moneyBack = moneyBack+(price*20/100)
             await customerModel.findOneAndUpdate({_id:userId},{$set:{moneyBack:moneyBack,category:"platinum"},$inc:{numberOfOrders:1}})
-            return res.status(201).send({status:true,message:`20% discount creditted to your account`,orderDetails:orderData})
+            return res.status(201).send({status:true,message:`20% discount credited to your account`,orderDetails:orderData})
         }
     } catch (error) {
         return res.status(500).send({status:false,message:error.message})
